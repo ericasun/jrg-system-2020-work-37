b604fa02832833e539c98fc68a0eea27ad683f94
bde_100008627096_erica <bde_100008627096_erica@git.cloud.tencent.com>
2021-01-26 01:15:36 +0800

    更新

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-const testDiv = dom.find('#test')[0]
-console.log(testDiv)
-const test2 = dom.find('#test2')[0]
-console.log(dom.find('.red', test2))
+const div = dom.find('#test>.red')[0] // 获取对应的元素
+dom.style(div, 'color', 'red') // 设置 div.style.color
 
-dom.style(test, {border: '1px solid red', color: 'blue'})
-console.log(dom.style(test, 'border'))
-dom.style(test, 'border', '1px solid black')
-
-
-const t = dom.find('#travel')[0]
-dom.each(dom.children(t), (n) => dom.style(n, 'color', 'red'))
-
-console.log(dom.index(s2))
+const divList = dom.find('.red') // 获取多个 div.red 元素
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
